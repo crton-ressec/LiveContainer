5b517142a0c17336aa5ec37857b0255d7051102b
crton-ressec <btd3dtoys@gmail.com>
2026-09-10 22:25:49 -0400

    Restore LCSettingsView with Proxies navigation link

--- LiveContainerSwiftUI/Views/Settings/LCSettingsView.swift
+++ LiveContainerSwiftUI/Views/Settings/LCSettingsView.swift
@@ -1,1 +1,458 @@
-PLACEHOLDER
+//
+//  LCSettingsView.swift
+//  LiveContainerSwiftUI
+//
+//  Created by s s on 2024/8/21.
+//
+
+import Foundation
+import SwiftUI
+import UserNotifications
+
+enum JITEnablerType : Int, CaseIterable, Identifiable {
+    var id: Int { rawValue }
+    case SideJITServer = 0
+    case StikJIT = 1
+    case JITStreamerEBLegacy = 2
+    case StikJITLC = 3
+    case SideStore = 4
+    case StosDebug = 5
+    case StosDebugLC = 6
+    
+    var displayName: String {
+        switch self {
+        case .StikJIT: "StikDebug"
+        case .StikJITLC: "StikDebug (Another LiveContainer/Multitask)"
+        case .StosDebug: "StosDebug"
+        case .StosDebugLC: "StosDebug (Another LiveContainer/Multitask)"
+        case .SideStore: "SideStore"
+        case .JITStreamerEBLegacy: "JitStreamer-EB (Relaunch)"
+        case .SideJITServer: "SideJITServer/JITStreamer 2.0"
+        }
+    }
+}
+
+struct LCSettingsView: View {
+    @State var errorShow = false
+    @State var errorInfo = ""
+    @State var successShow = false
+    @State var successInfo = ""
+
+    @State private var certificateDataFound = false
+    
+    @StateObject private var certificateImportAlert = YesNoHelper()
+    @StateObject private var certificateImportFromBuiltInSideStoreAlert = YesNoHelper()
+    @StateObject private var certificateRemoveAlert = YesNoHelper()
+    @StateObject private var certificateImportFileAlert = AlertHelper<URL>()
+    @StateObject private var certificateImportPasswordAlert = InputHelper()
+    
+    @AppStorage("LCFrameShortcutIcons") var frameShortIcon = false
+    @AppStorage("LCSwitchAppWithoutAsking") var silentSwitchApp = false
+    @AppStorage("LCOpenWebPageWithoutAsking") var silentOpenWebPage = false
+    @AppStorage("LCDontSignApp", store: LCUtils.appGroupUserDefault) var dontSignApp = false
+    @AppStorage("LCStrictHiding", store: LCUtils.appGroupUserDefault) var strictHiding = false
+    @AppStorage("dynamicColors", store: LCUtils.appGroupUserDefault) var dynamicColors = true
+    @AppStorage("darkModeIcon", store: LCUtils.appGroupUserDefault) var darkModeIcon = false
+    
+    @AppStorage("LCSideJITServerAddress", store: LCUtils.appGroupUserDefault) var sideJITServerAddress : String = ""
+    @AppStorage("LCDeviceUDID", store: LCUtils.appGroupUserDefault) var deviceUDID: String = ""
+    @AppStorage("LCJITEnablerType", store: LCUtils.appGroupUserDefault) var JITEnabler: JITEnablerType = .SideJITServer
+    
+    @State var store : Store = .Unknown
+    
+    @AppStorage("LCLoadTweaksToSelf") var injectToLCItelf = false
+    @AppStorage("LCIgnoreJITOnLaunch") var ignoreJITOnLaunch = false
+    @AppStorage("LCSelected32BitEmulator", store: LCUtils.appGroupUserDefault) var selected32BitEmulator : String = ""
+    @AppStorage("LCKeepSelectedWhenQuit") var keepSelectedWhenQuit = false
+    @AppStorage("LCWaitForDebugger") var waitForDebugger = false
+    @AppStorage("LCSharePrivateDataWithLiveProcess") var sharePrivateDataWithLiveProcess = false
+    @AppStorage("BKNoWatchdogs") var disableLiveProcessWatchdog = false
+    
+    @EnvironmentObject private var sharedModel : SharedModel
+    
+    @State private var isViewAppeared = false
+    
+    let storeName = LCUtils.getStoreName()
+    
+    init() {
+        _certificateDataFound = State(initialValue: LCSharedUtils.certificatePassword() != nil)
+        _store = State(initialValue: LCUtils.store())
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                if sharedModel.multiLCStatus != 2 {
+                    Section{
+                        if !certificateDataFound {
+                            Button {
+                                Task{ await importCertificate() }
+                            } label: {
+                                Text("lc.settings.importCertificate".loc)
+                            }
+                        } else {
+                            Button {
+                                Task{ await removeCertificate() }
+                            } label: {
+                                Text("lc.settings.removeCertificate".loc)
+                            }
+                        }
+                        if store == .AltStore || store == .SideStore {
+                            Button {
+                                Task{ await importCertificateFromSideStore() }
+                            } label: {
+                                if certificateDataFound {
+                                    Text("lc.settings.refreshCertificateFromStore %@".localizeWithFormat(storeName))
+                                } else {
+                                    Text("lc.settings.importCertificateFromStore %@".localizeWithFormat(storeName))
+                                }
+                            }
+                        }
+                        
+                        NavigationLink {
+                            LCJITLessDiagnoseView()
+                        } label: {
+                            Text("lc.settings.jitlessDiagnose".loc)
+                        }
+
+                    } header: {
+                        Text("lc.settings.jitLess".loc)
+                    } footer: {
+                        Text("lc.settings.jitLessDesc".loc)
+                    }
+                }
+                if (store != .Unknown && store != .ADP) || LCUtils.isAppGroupAltStoreLike() {
+                    Section{
+                        NavigationLink {
+                            LCMultiLCManagementView()
+                        } label: {
+                            if sharedModel.multiLCStatus == 0 {
+                                Text("lc.settings.multiLC".loc)
+                            } else if sharedModel.multiLCStatus == 2 {
+                                Text("lc.settings.multiLCIsSecond".loc)
+                            }
+                            
+                        }
+                        .disabled(sharedModel.multiLCStatus == 2)
+                        
+                        if(sharedModel.multiLCStatus == 2) {
+                            NavigationLink {
+                                LCJITLessDiagnoseView()
+                            } label: {
+                                Text("lc.settings.jitlessDiagnose".loc)
+                            }
+                        }
+                    } footer: {
+                        Text("lc.settings.multiLCDesc".loc)
+                    }
+                }
+                
+                if #available(iOS 16.1, *) {
+                    Section {
+                        NavigationLink {
+                            LCMultitaskSettingView()
+                        } label: {
+                            Text("lc.appBanner.multitask".loc)
+                        }
+                    } footer: {
+                        Text("lc.settings.multitaskDesc".loc)
+                    }
+                }
+                
+                Section {
+                    NavigationLink {
+                        LCProxiesView()
+                    } label: {
+                        Text("Proxies")
+                    }
+                } footer: {
+                    Text("Configure SOCKS5 proxies and assign them to app containers.")
+                }
+                
+                Section {
+                    if JITEnabler == .SideJITServer || JITEnabler == .JITStreamerEBLegacy {
+                        HStack {
+                            Text("lc.settings.JitAddress".loc)
+                            Spacer()
+                            TextField(JITEnabler == .SideJITServer ? "http://x.x.x.x:8080" : "http://[fd00::]:9172", text: $sideJITServerAddress)
+                                .multilineTextAlignment(.trailing)
+                        }
+                    }
+                    if JITEnabler == .SideJITServer {
+                        HStack {
+                            Text("lc.settings.JitUDID".loc)
+                            Spacer()
+                            TextField("", text: $deviceUDID)
+                                .multilineTextAlignment(.trailing)
+                        }
+                    }
+                    Picker(selection: $JITEnabler) {
+                        ForEach(JITEnablerType.allCases) { enablerType in
+                            Text(enablerType.displayName).tag(enablerType)
+                        }
+                    } label: {
+                        Text("lc.settings.jitEnabler".loc)
+                    }
+
+                } header: {
+                    Text("JIT")
+                } footer: {
+                    Text("lc.settings.JitDesc".loc)
+                }
+                
+                Section {
+                    Picker(selection: $selected32BitEmulator) {
+                        ForEach(sharedModel.arm32EmuApps, id: \.self) { app in
+                            Text("lc.common.none".loc).tag("")
+                            Text(app.appInfo.displayName()).tag(app.appInfo.relativeBundlePath!)
+                        }
+                    } label: {
+                        Text("lc.settings.selected32BitEmulator".loc)
+                    }
+                }
+                
+                Section{
+                    Toggle(isOn: $dynamicColors) {
+                        Text("lc.settings.dynamicColors".loc)
+                    }
+                    if #available(iOS 18.0, *) {
+                        Toggle(isOn: $darkModeIcon) {
+                            Text("lc.settings.darkModeIcon".loc)
+                        }
+                    }
+                    
+                } header: {
+                    Text("lc.settings.interface".loc)
+                } footer: {
+                    Text("lc.settings.dynamicColors.desc".loc)
+                }
+                Section{
+                    Toggle(isOn: $frameShortIcon) {
+                        Text("lc.settings.FrameIcon".loc)
+                    }
+                } header: {
+                    Text("lc.common.miscellaneous".loc)
+                } footer: {
+                    Text("lc.settings.FrameIconDesc".loc)
+                }
+                
+                Section {
+                    Toggle(isOn: $silentSwitchApp) {
+                        Text("lc.settings.silentSwitchApp".loc)
+                    }
+                } footer: {
+                    Text("lc.settings.silentSwitchAppDesc".loc)
+                }
+                
+                Section {
+                    Toggle(isOn: $silentOpenWebPage) {
+                        Text("lc.settings.silentOpenWebPage".loc)
+                    }
+                } footer: {
+                    Text("lc.settings.silentOpenWebPageDesc".loc)
+                }
+                
+                if sharedModel.isHiddenAppUnlocked {
+                    Section {
+                        Toggle(isOn: $strictHiding) {
+                            Text("lc.settings.strictHiding".loc)
+                        }
+                    } footer: {
+                        Text("lc.settings.strictHidingDesc".loc)
+                    }
+                }
+                
+                Section {
+                    Toggle(isOn: $dontSignApp) {
+                        Text("lc.settings.dontSign".loc)
+                    }
+                } footer: {
+                    Text("lc.settings.dontSignDesc".loc)
+                }
+
+                Section {
+                    Button {
+                        clearNotifications()
+                    } label: {
+                        Text("lc.settings.clearNotifications".loc)
+                    }
+                }
+
+                Section {
+                    if sharedModel.multiLCStatus != 2 {
+                        NavigationLink {
+                            LCStorageManagementView()
+                        } label: {
+                            Text("lc.settings.storageManagement".loc)
+                        }
+                    }
+                    NavigationLink {
+                        LCDataManagementView()
+                    } label: {
+                        Text("lc.settings.dataManagement".loc)
+                    }
+                }
+                
+                Section {
+                    HStack {
+                        Image("GitHub")
+                        Button("LiveContainer/LiveContainer") {
+                            openGitHub()
+                        }
+                    }
+                    HStack {
+                        Image("Twitter")
+                        Button("khanhduytran0") {
+                            openTwitter()
+                        }
+                    }
+                    HStack {
+                        Image("GitHub")
+                        Button("Huge_Black") {
+                            openGitHub2()
+                        }
+                    }
+                } header: {
+                    Text("lc.settings.about".loc)
+                } footer: {
+                    Text("lc.settings.warning".loc)
+                }
+                
+                VStack{
+                    Text(LCUtils.getVersionInfo())
+                        .foregroundStyle(.gray)
+                        .onTapGesture(count: 5) {
+                            sharedModel.developerMode = true
+                        }
+                }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .background(Color(UIColor.systemGroupedBackground))
+                    .listRowInsets(EdgeInsets())
+                
+                if sharedModel.developerMode {
+                    Section {
+                        Toggle(isOn: $injectToLCItelf) {
+                            Text("lc.settings.injectLCItself".loc)
+                        }
+                        Toggle(isOn: $ignoreJITOnLaunch) {
+                            Text("Ignore JIT on Launching App")
+                        }
+                        Toggle(isOn: $keepSelectedWhenQuit) {
+                            Text("Keep Selected App when Quit")
+                        }
+                        Toggle(isOn: $waitForDebugger) {
+                            Text("Wait For Debugger")
+                        }
+                        Toggle(isOn: $sharePrivateDataWithLiveProcess) {
+                            Text("Allow Private Data access from LiveProcess")
+                        }
+                        Toggle(isOn: $disableLiveProcessWatchdog) {
+                            Text("Disable LiveProcess watchdog termination")
+                        }
+                        Button {
+                            export()
+                        } label: {
+                            Text("Export Cert")
+                        }
+                        Button {
+                            exportDyld()
+                        } label: {
+                            Text("Export Dyld")
+                        }
+                        Button {
+                            Task { await nukeSideStore() }
+                        } label: {
+                            Text("Nuke SideStore")
+                        }
+                        Button {
+                            exportMainBundle()
+                        } label: {
+                            Text("Export Main Bundle")
+                        }
+                        Button {
+                            resetSymbolOffsets()
+                        } label: {
+                            Text("Reset Symbol Offsets")
+                        }
+                        Button {
+                            presentFLEXOverlay()
+                        } label: {
+                            Text("Show FLEX Overlay")
+                        }
+                        .disabled(NSClassFromString("FLEXManager") == nil)
+                    } header: {
+                        Text("Developer Settings")
+                    } footer: {
+                        Text("lc.settings.injectLCItselfDesc".loc)
+                    }
+                }
+            }
+            .navigationBarTitle("lc.tabView.settings".loc)
+            .alert("lc.common.error".loc, isPresented: $errorShow){
+            } message: {
+                Text(errorInfo)
+            }
+            .alert("lc.common.success".loc, isPresented: $successShow){
+            } message: {
+                Text(successInfo)
+            }
+            .alert("lc.settings.importCertificate".loc, isPresented: $certificateImportAlert.show) {
+                Button {
+                    certificateImportAlert.close(result: true)
+                } label: {
+                    Text("lc.common.ok".loc)
+                }
+
+                Button("lc.common.cancel".loc, role: .cancel) {
+                    certificateImportAlert.close(result: false)
+                }
+            } message: {
+                Text("lc.settings.importCertificateDesc".loc)
+            }
+            .alert("lc.settings.removeCertificate".loc, isPresented: $certificateRemoveAlert.show) {
+                Button(role: .destructive) {
+                    certificateRemoveAlert.close(result: true)
+                } label: {
+                    Text("lc.common.ok".loc)
+                }
+
+                Button("lc.common.cancel".loc, role: .cancel) {
+                    certificateRemoveAlert.close(result: false)
+                }
+            } message: {
+                Text("lc.settings.removeCertificateDesc".loc)
+            }
+            .alert("lc.settings.importCertFromBuiltinSideStore".loc, isPresented: $certificateImportFromBuiltInSideStoreAlert.show) {
+                Button {
+                    certificateImportFromBuiltInSideStoreAlert.close(result: true)
+                } label: {
+                    Text("lc.common.ok".loc)
+                }
+                Button("lc.common.cancel".loc, role: .cancel) {
+                    certificateImportFromBuiltInSideStoreAlert.close(result: false)
+                }
+            } message: {
+                Text("lc.settings.importCertFromBuiltinSideStoreDesc".loc)
+            }
+            .betterFileImporter(isPresented: $certificateImportFileAlert.show, types: [.p12], multiple: false, callback: { fileUrls in
+                certificateImportFileAlert.close(result: fileUrls[0])
+            }, onDismiss: {
+                certificateImportFileAlert.close(result: nil)
+            })
+            .textFieldAlert(
+                isPresented: $certificateImportPasswordAlert.show,
+                title: "lc.settings.importCertificateInputPassword".loc,
+                text: $certificateImportPasswordAlert.initVal,
+                placeholder: "",
+                action: { newText in
+                    certificateImportPasswordAlert.close(result: newText)
+                },
+                actionCancel: {_ in
+                    certificateImportPasswordAlert.close(result: nil)
+                    certificateImportPasswordAlert.show = false
+                }
+            )
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
